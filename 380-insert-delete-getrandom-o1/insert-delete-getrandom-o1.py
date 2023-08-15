05f1515d4e8959ddf886fd1c6f0623872eb4279b
Marcel Ruth <72654154+MarcelRuth@users.noreply.github.com>
2023-08-16 00:13:50 +0200

    Time: 361 ms (80.73%) | Memory: 63.6 MB (74.01%) - LeetSync

--- 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
+++ 380-insert-delete-getrandom-o1/insert-delete-getrandom-o1.py
@@ -1,24 +1,27 @@
 class RandomizedSet:
 
     def __init__(self):
-        self.s = []
-
+        self.s_map = {} # check for values
+        # checking in map is faster than in list
+        self.s = [] # store values
     def insert(self, val: int) -> bool:
-        if val in self.s:
+        if val in self.s_map:
             return False
         else:
+            self.s_map[val] = len(self.s) # index of value
             self.s.append(val)
             return True
 
     def remove(self, val: int) -> bool:
-        if val in self.s:
+        if val in self.s_map:
+            del self.s_map[val]
             self.s.remove(val)
             return True
         else:
             return False
 
     def getRandom(self) -> int:
-        return self.s[random.randint(0, len(self.s)-1)]
+        return random.choice(self.s)
 
 
 # Your RandomizedSet object will be instantiated and called as such:
