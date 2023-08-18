ff134f15b8719889a4696753405419db39e8919b
Marcel Ruth <72654154+MarcelRuth@users.noreply.github.com>
2023-08-18 23:30:27 +0200

    Time: 309 ms (81.67%) | Memory: 18 MB (82.97%) - LeetSync

--- 1738-maximal-network-rank/maximal-network-rank.py
+++ 1738-maximal-network-rank/maximal-network-rank.py
@@ -3,25 +3,27 @@ class Solution:
 
         # it is a graph and we are looking for two connected nodes
         # that have the most edges attached to them
-        counter = {}
+        counter = [0] * n
         max_rank = 0
 
-        for i in range(n):
-            counter[i] = 0
+        # used to check for connections later
+        graph = [[False] * n for _ in range(n)]
 
         for edge in roads:
             # first count the edges of each node
             counter[edge[0]] += 1
             counter[edge[1]] += 1
 
+            # set edges in the graph
+            graph[edge[0]][edge[1]] = graph[edge[1]][edge[0]] = True
+
+
         # check all combinations
-        for key in counter:
-            for key2 in counter:
-                if key != key2:
-                    if [key, key2] in roads or [key2, key] in roads:
-                        pot_max_rank = counter[key] + counter[key2] - 1
-                    else:
-                        pot_max_rank = counter[key] + counter[key2]
-                    max_rank = max(max_rank, pot_max_rank)
+        for i in range(n):
+            for j in range(i + 1, n):
+                pot_max_rank = counter[i] + counter[j] 
+                if graph[i][j]: # connected nodes
+                    pot_max_rank -= 1
+                max_rank = max(max_rank, pot_max_rank)
 
         return max_rank
