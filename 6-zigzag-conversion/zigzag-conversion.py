2128f2287a57a367d85b1db9e0ec48b67805935b
Marcel Ruth <72654154+MarcelRuth@users.noreply.github.com>
2023-09-01 15:23:16 +0200

    Time: 48 ms (97.06%) | Memory: 16.5 MB (56.78%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -1,46 +1,70 @@
 class Solution:
     def convert(self, s: str, numRows: int) -> str:
+
+        # intuitive way
+        # if numRows == 1:
+        #     return s
+        # if numRows == len(s):
+        #     return s
+        # # add lists to this list
+        # # ending up with a matrix and lists as columns
+
+        # string_matrix = []
+        # left_counter = 0
+        # right_counter = numRows
+        # num_columns = 0
+        # while left_counter < len(s):
+        #     i = num_columns
+        #     # full column
+        #     if left_counter >= len(s):
+        #         left_counter = -1
+
+        #     if i % (numRows - 1) == 0:
+        #         string_matrix.append([s[left_counter:right_counter]])
+        #         left_counter += numRows
+        #         right_counter += numRows
+
+        #     # diagonal
+        #     else:
+        #         filler_string_up = '$' * (1 + numRows - 2 - (i % (numRows - 1))) 
+        #         filler_string_low = '$' * ((numRows) - len(filler_string_up) - 1)
+        #         string_matrix.append([f'{filler_string_up}{s[left_counter]}{filler_string_low}'])
+        #         left_counter += 1
+        #         right_counter += 1
+
+        #     num_columns += 1
+        # print(string_matrix)
+
+        # final_string = ''
+        # for i in range(numRows):
+        #     for j in range(num_columns):
+        #         # if last column, fill with $
+        #         if j == num_columns - 1:
+        #             filler = '$' * (numRows - len(string_matrix[j]))
+        #             string_matrix[j][0] += filler
+        #         final_string += string_matrix[j][0][i]
+
+        # return final_string.replace('$', '')
+
+        # fast way
+        
         if numRows == 1:
             return s
-        if numRows == len(s):
-            return s
-        # add lists to this list
-        # ending up with a matrix and lists as columns
+            
+        row_arr = [""] * numRows
+        row_idx = 1
+        going_up = True
 
-        string_matrix = []
-        left_counter = 0
-        right_counter = numRows
-        num_columns = 0
-        while left_counter < len(s):
-            i = num_columns
-            # full column
-            if left_counter >= len(s):
-                left_counter = -1
-
-            if i % (numRows - 1) == 0:
-                string_matrix.append([s[left_counter:right_counter]])
-                left_counter += numRows
-                right_counter += numRows
-
-            # diagonal
+        for ch in s:
+            row_arr[row_idx-1] += ch
+            if row_idx == numRows:
+                going_up = False
+            elif row_idx == 1:
+                going_up = True
+            
+            if going_up:
+                row_idx += 1
             else:
-                filler_string_up = '$' * (1 + numRows - 2 - (i % (numRows - 1))) 
-                filler_string_low = '$' * ((numRows) - len(filler_string_up) - 1)
-                string_matrix.append([f'{filler_string_up}{s[left_counter]}{filler_string_low}'])
-                left_counter += 1
-                right_counter += 1
-
-            num_columns += 1
-        print(string_matrix)
-
-        final_string = ''
-        for i in range(numRows):
-            for j in range(num_columns):
-                # if last column, fill with $
-                if j == num_columns - 1:
-                    filler = '$' * (numRows - len(string_matrix[j]))
-                    string_matrix[j][0] += filler
-                final_string += string_matrix[j][0][i]
-
-        return final_string.replace('$', '')
-
+                row_idx -= 1
+        
+        return "".join(row_arr)
