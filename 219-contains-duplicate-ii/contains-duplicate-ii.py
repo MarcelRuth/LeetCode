ae35944d484d9697bfa731dcc50ba7373ad56efe
Marcel Ruth <72654154+MarcelRuth@users.noreply.github.com>
2023-09-13 21:18:53 +0200

    Time: 507 ms (91.14%) | Memory: 29.5 MB (68.34%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.py
+++ 219-contains-duplicate-ii/contains-duplicate-ii.py
@@ -1,10 +1,17 @@
-from collections import deque
+# from collections import deque
 class Solution:
     def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
         # deque approach
-        q = deque([], k)
-        for n in nums:
-            if n in q:
+        # slow but low memory
+        # q = deque([], k)
+        # for n in nums:
+        #     if n in q:
+        #         return True
+        #     q.append(n)
+        # return False
+        hs = {}
+        for i, n in enumerate(nums):
+            if n in hs and abs(i - hs[n]) <= k:
                 return True
-            q.append(n)
+            hs[n] = i
         return False
