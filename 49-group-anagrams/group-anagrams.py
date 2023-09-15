0e72aca0c28ddf429a85ec4f3b5eb134516e6197
Marcel Ruth <72654154+MarcelRuth@users.noreply.github.com>
2023-09-15 21:44:46 +0200

    Time: 96 ms (76.73%) | Memory: 19.6 MB (84.03%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,16 +1,25 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        final_list = []
-        counters = {}
 
+        # final_list = []
+        # counters = {}
+
+        # for s in strs:
+        #     counter_tuple = tuple(sorted(Counter(s).items()))
+        #     if counter_tuple in counters:
+        #         counters[counter_tuple].append(s)
+        #     else:
+        #         counters[counter_tuple] = [s]
+        
+        # for key in counters:
+        #     final_list.append(counters[key])
+        
+        # return final_list
+
+        # faster by sorting the strings and then using that as key in the map
+        mp = defaultdict(list)
         for s in strs:
-            counter_tuple = tuple(sorted(Counter(s).items()))
-            if counter_tuple in counters:
-                counters[counter_tuple].append(s)
-            else:
-                counters[counter_tuple] = [s]
+            sorted_s = ''.join(sorted(s))
+            mp[sorted_s].append(s)
         
-        for key in counters:
-            final_list.append(counters[key])
-        
-        return final_list
+        return list(mp.values())
