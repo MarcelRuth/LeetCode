67c7d2a25f6279cc34efc313cea64862a18a9ba2
Marcel Ruth <72654154+MarcelRuth@users.noreply.github.com>
2023-09-18 17:17:12 +0200

    Time: 69 ms (15.02%) | Memory: 20.2 MB (97.19%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.py
+++ 141-linked-list-cycle/linked-list-cycle.py
@@ -6,12 +6,21 @@
 
 class Solution:
     def hasCycle(self, head: Optional[ListNode]) -> bool:
-        mapping = set()
-        while head not in mapping:
-            if head == None:
-                return False
-            mapping.add(head)
-            head = head.next
-        return True
+        #mapping = set()
+        #while head not in mapping:
+        #    if head == None:
+        #       return False
+        #    mapping.add(head)
+        #    head = head.next
+        #return True
 
-        
+    # "hare and tortoise" algorithm
+        fast_pointer = head
+        slow_pointer = head
+
+        while fast_pointer and fast_pointer.next:
+            slow_pointer = slow_pointer.next
+            fast_pointer = fast_pointer.next.next
+            if fast_pointer == slow_pointer:
+                return True
+        return False
