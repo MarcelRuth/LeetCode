27328cb4e02d33111f3fb478fa577e8a2c11a949
Marcel Ruth <72654154+MarcelRuth@users.noreply.github.com>
2023-09-23 22:40:34 +0200

    Time: 41 ms (42.67%) | Memory: 16.2 MB (92.76%) - LeetSync

---
+++ 50-powx-n/powx-n.py
@@ -0,0 +1,18 @@
+class Solution:
+    def myPow(self, x: float, n: int) -> float:
+        # divide the work using binary representation of exponents
+
+        # invert as x^-1 = 1/x ...
+        if  n < 0:
+            n = -n
+            x = 1 / x
+
+        res = 1
+        while n != 0:
+            if (n % 2) != 0:
+                res *= x
+            
+            x *= x
+            n = n // 2
+
+        return res
